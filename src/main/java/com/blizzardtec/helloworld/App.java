24fd9087a40f05267142d3da8641ca9d5c96aae4
barnabyg <barnabyg@blizzardtec.com>
2014-06-07 15:14:41 +0100

    updated

--- src/main/java/com/blizzardtec/helloworld/App.java
+++ src/main/java/com/blizzardtec/helloworld/App.java
@@ -4,24 +4,35 @@ package com.blizzardtec.helloworld;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
+public final class App {
+    /**
+     * blah.
+     * @param args param
+     */
+    public static void main(final String[] args) {
         final App app = new App();
         app.printHello();
         app.printSpace();
         app.printWorld();
     }
 
+    /**
+     * blah.
+     */
     protected void printHello() {
         System.out.print("Hello");
     }
 
+    /**
+     * blah.
+     */
     protected void printWorld() {
         System.out.print("World");
     }
 
+    /**
+     * blah.
+     */
     protected void printSpace() {
         System.out.print(" ");
     }
